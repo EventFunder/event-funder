987c89946c728990dcf2a4e86485b6d14047a015
dangitsvi <dangitsvi@gmail.com>
2015-07-27 15:13:36 -0700

    added all routes

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -4,10 +4,31 @@ var bodyParser = require('body-parser');
 module.exports = function(router) {
   router.use(bodyParser.json());
 
+  function successRes(req, res) {
+    res.json({'msg': 'route successfully reached'});
+  }
+
   router.route('/')
-    .get (function(req, res) {
-      res.json({'msg': 'got index'});
-    });
+    .get(successRes)
+    .post(successRes);
 
+  router.route('/:user')
+    .get(successRes);
 
+  router.route('/:user/events')
+    .get(successRes)
+    .post(successRes);
+
+  router.route('/:user/events/:event')
+    .get(successRes)
+    .put(successRes)
+    .delete(successRes);
+
+  router.route('/:user/events/:event/committers')
+    .get(successRes)
+    .post(successRes);
+
+  router.route('/:user/events/:event/committers/:committer')
+    .get(successRes)
+    .delete(successRes);
 }
--- server.js
+++ server.js
@@ -14,9 +14,9 @@ var userRouter = express.Router();
 require('./backend/routes/user-routes')(userRouter);
 app.use('/', userRouter);
 
-// app.all('*', function(res, req) {
-//   res.status(404).json({'msg': '404 error, you done fucked up son'});
-// });
+app.all('*', function(req, res) {
+  res.status(404).json({'msg': '404 error, you done fucked up son'});
+});
 
 app.listen(port, function() {
   console.log('Server listening at port: ' + port);
