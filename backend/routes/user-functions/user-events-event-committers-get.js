aaa9c5e983644a16cd8954b562f587dc057e7fe2
dangitsvi <dangitsvi@gmail.com>
2015-07-29 15:38:12 -0700

    added all routes

--- backend/routes/user-functions/user-events-event-committers-get.js
+++ backend/routes/user-functions/user-events-event-committers-get.js
@@ -1,3 +1,13 @@
+var Committer = require('../../models/Committer');
+
 module.exports = function (req, res) {
-  res.json({'msg': 'route successfully reached'});
+  var eventId = req.params.event;
+
+  Committer.find({eventToAttend: eventId}, function(err, committers) {
+    if(err) {
+      res.status(500).json({msg: 'Server error: could not get Comitters'});
+    } else {
+      res.json({msg: 'Committers array recieved', committers: committers});
+    }
+  });
 }
