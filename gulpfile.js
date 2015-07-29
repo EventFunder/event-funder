a785335fd5020b8408eb95e53d680f5ada385019
Farhad Ahmed <farhadahmed2007@gmail.com>
2015-07-29 11:51:45 -0700

    In the process of fixing merge errors. File not yet working

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,71 @@
-// <<<<<<< HEAD
-// 'use strict';
+'use strict';
 
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-// var webpack = require('gulp-webpack');
-// var uglify = require('gulp-uglify');
-// var minifyCss = require('gulp-minify-css');
-// var minifyHTML = require('gulp-minify-html');
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('jshint');
+var sass = require('gulp-sass');
+var webpack = require('gulp-webpack');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
 
-// gulp.task('sass', function () {
-//   gulp.src('./frontend/app/sass/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(minifyCss({compatibility: 'ie8'}))
-//     .pipe(gulp.dest('./public/css'));
-// });
+gulp.task('default', ['test', 'lint'],function() {});
 
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./frontend/app/sass/**/*.scss', ['sass']);
-// });
+gulp.task('test', function() {
+  gulp.src('./backend/tests/test.js')
+      .pipe(mocha());
+});
 
-// gulp.task('webpackdev', function() {
-//   return gulp.src('./frontend/app/js/**/*.js')
-//     .pipe(webpack({
-//       output: {
-//         filename: 'bundle.js'
-//       }
-//     }))
-//     // .pipe(uglify())
-//     .pipe(gulp.dest('public/'));
-// });
-// gulp.task('webpackdev:watch', function () {
-//   gulp.watch('./frontend/app/**/*.js', ['webpackdev']);
-// });
+gulp.task('lint', function() {
+  gulp.src('*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+});
 
-// gulp.task('copy', function() {
-//   var opts = {
-//     conditionals: true,
-//     spare:true
-//   };
+gulp.task('sass', function () {
+  gulp.src('./frontend/app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./public/css'));
+});
 
-//   return gulp.src('./frontend/app/**/*.html')
-//     .pipe(gulp.dest('public/'))
-//     .pipe(minifyHTML(opts))
-//     .pipe(gulp.dest('./public/'));
-// });
+gulp.task('sass:watch', function () {
+  gulp.watch('./frontend/app/sass/**/*.scss', ['sass']);
+});
 
-// gulp.task('copy:watch', function () {
-//   gulp.watch('./frontend/app/**/*.html', ['copy']);
-// });
+gulp.task('webpackdev', function() {
+  return gulp.src('./frontend/app/js/**/*.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    // .pipe(uglify())
+    .pipe(gulp.dest('public/'));
+});
+gulp.task('webpackdev:watch', function () {
+  gulp.watch('./frontend/app/**/*.js', ['webpackdev']);
+});
 
-// gulp.task('build', ['copy','webpackdev','sass','sass:watch','copy:watch', 'webpackdev:watch']);
-// gulp.task('default', ['build']);
-// =======
-// var gulp = require('gulp');
-// var mocha = require('gulp-mocha');
-// var jshint = require('gulp-jshint');
+gulp.task('copy', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
 
-// gulp.task('default', ['test', 'lint'],function() {});
+  return gulp.src('./frontend/app/**/*.html')
+    .pipe(gulp.dest('public/'))
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./public/'));
+});
 
-// gulp.task('test', function() {
-//   gulp.src('./backend/tests/test.js')
-//       .pipe(mocha());
-// });
+gulp.task('copy:watch', function () {
+  gulp.watch('./frontend/app/**/*.html', ['copy']);
+});
 
-// gulp.task('lint', function() {
-//   gulp.src('*.js')
-//       .pipe(jshint())
-//       .pipe(jshint.reporter('default'));
-// });
+gulp.task('build', ['copy','webpackdev','sass','sass:watch','copy:watch', 'webpackdev:watch']);
 
-// gulp.task('watch', function() {
-//   gulp.watch('*.js', ['test', 'lint']);
-// });
-// >>>>>>> master
+
+gulp.task('watch', function() {
+  gulp.watch('*.js', ['test', 'lint']);
+});
