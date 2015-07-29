4b6b125dbde360922f1b7e80d0a5be8066951989
dangitsvi <dangitsvi@gmail.com>
2015-07-29 12:43:15 -0700

    clean up

--- backend/routes/user-functions/user-events-event-delete.js
+++ backend/routes/user-functions/user-events-event-delete.js
@@ -4,6 +4,7 @@ module.exports = function (req, res) {
 
   var eventId = req.params.event;
 
+  //check to see if user owns this event
   Event.findById(eventId, function(err, eventData) {
     if(err) {
       res.status(500).json({msg: 'Server error: cannot find event'});
