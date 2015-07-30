822b4f604d81285540d3a61b99ccb6a46b927686
Andre Dinh <dinh.andre@yahoo.com>
2015-07-29 21:48:09 -0700

    omg its showing

--- public/bundle.js
+++ public/bundle.js
@@ -29821,9 +29821,11 @@
 	      console.log(responseKey);
 	      // $http.defaults.headers.common['x-access-token'] = responseKey;
 	      $http.get('/user/events/').success(function(response){
-	        console.log('Got some data');
-	        console.log(response);
-	        $scope.event = response;
+	        // console.log('Got some data');
+	        // console.log(response);
+	        $scope.events = response.events;
+	        // console.log('response.events ' + response.events);
+	        // console.log('response.events[0] '+ response.events[0].name);
 	        $scope.event ='';
 	      });
 	    };
